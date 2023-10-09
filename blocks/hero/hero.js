54d4a8251a1e8d5d606a52f67995565fc5ec08d5
Lamont Crook <lamont@adobe.com>
2023-10-09 12:01:50 -0600

    add small header

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,8 @@
+import { getMetadata } from '../../scripts/aem.js';
+
 export default function decorate(block) {
   const h1 = block.querySelector('h1');
+  const theme = getMetadata('theme');
   const picture = block.querySelector('picture');
   const h2 = block.querySelector('h2');
   const p = block.querySelectorAll('p');
@@ -15,7 +18,9 @@ export default function decorate(block) {
   block.querySelector('div').append(heroContent);
   block.querySelector('div').querySelector('div').remove();
 
-  const blue = document.createElement('div');
-  blue.classList.add('blue-box');
-  block.append(blue);
+  if (theme !== 'small-hero') {
+    const blue = document.createElement('div');
+    blue.classList.add('blue-box');
+    block.append(blue);
+  }
 }
